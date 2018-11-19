2c0f0a41c4371714e75df8f00a602c5b94f0b8d5
이규현 <qbbang@gmail.com>
2018-11-19 12:04:21 +0900

    최종본

---
+++ HackerTonProjectTemp/LeeKimTeam/ect.swift
@@ -0,0 +1,17 @@
+//final class Annotation: NSObject, MKAnnotation {
+//    var title: String?
+//    var coordinate: CLLocationCoordinate2D
+//
+//    init(title: String, coordinate: CLLocationCoordinate2D) {
+//        self.title = title
+//        self.coordinate = coordinate
+//    }
+//}
+
+//final class CameraInfo: MKPointAnnotation {
+//    var exhibition : [String]!
+//    var phoneNumber : String!
+//    var url : URL!
+//}
+//
+
---
+++ HackerTonProjectTemp/LeeKimTeam/xmldata.swift
@@ -0,0 +1,25 @@
+//
+//  xmlData.swift
+//  StanfordFaker
+//
+//  Created by qbbang on 16/11/2018.
+//  Copyright © 2018 qbbang. All rights reserved.
+//
+
+import Foundation
+
+struct xmlInfo {
+  var roadAddress = Array<String>() //<소재지도로명주소>
+  var installationPurpose = Array<String>() //<설치목적구분>교통단속
+  var latitude = Array<String>() //<위도>37.55007553</위도>
+  var longitude = Array<String>() //<경도>127.0551224</경도>
+}
+
+
+struct TrafficInfo {
+  var roadAddress = Array<String>() //<소재지도로명주소>
+  var installationPurpose = Array<String>() //<설치목적구분>교통단속
+  var latitude = Array<String>() //<위도>37.55007553</위도>
+  var longitude = Array<String>() //<경도>127.0551224</경도>
+}
+
